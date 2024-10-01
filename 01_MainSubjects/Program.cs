4a4336ce3401870b923436d91feb58c8c28b5279
seymanrcn <cinseymanur@gmail.com>
2024-10-01 21:14:41 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            #region YazdırmaKomutları
+
+            //Console.Write("merhaba dünya");
+            //Console.Read();
+            //Console.WriteLine("***** Yemek kategorileri *****"); //imleci alt satıra indirir.
+            //Console.WriteLine();
+            //Console.WriteLine("1-çorbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Salatalar");
+            //Console.WriteLine("4-İçecekler");
+            //Console.WriteLine("5-Tatlılar);
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yemek Kategorileri ****** ");
+            //Console.Read();
+
+
+            #endregion
+
+            #region StringDeğişkenler
+
+            //string 
+            //değişkenin_türü değişken_adı 
+            //string name;
+            //name = "Murat";
+            //Console.Write(name);
+
+
+            //string customerName;
+            //string customerSurname;
+            //string customerEmail,district,city;
+            //string customerPhone;
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+            //customerEmail = "deneme@ggmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+            //customerPhone = "+90 555 55 55";
+            //Console.WriteLine("***** Rezervasyon kartı *****");
+            //Console.WriteLine();
+
+            //Console.WriteLine("-------------------------------------------------");
+            //Console.WriteLine("Müşteri : " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim :" + customerPhone);
+            //Console.WriteLine("Email Adresi:" + customerEmail);
+            //Console.WriteLine("Adres :" + district + "/" + city);
+            //Console.WriteLine("-------------------------------------------------");
+
+
+            #endregion
+
+
+            #region Int Değişkenler
+            //int
+
+            int number = 24;
+            Console.WriteLine(number);
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            Console.Read();
+
+
+        }
+    }
+}
+
+
---
+++ 02_variables/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Double Değişkenler
+            //double number;
+            //number = 4.85;
+
+            //Console.WriteLine(number);
+
+            #endregion
+
+            #region klavyeden veri girişleri string değişkenler
+            //Console.WriteLine("***** csharp havayolları yolcu bilgisi");
+            //Console.WriteLine();
+
+            //string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge,
+            //    passengerIdentityNumber;
+
+            //Console.Write("yolcu adı :");
+            //passengerName=Console.ReadLine();
+
+            #endregion
+
+
+            #region klavyeden tam sayı girişleri ve dönüşümler
+            //int shoePrice, computerPrice, tvPrice;
+            //shoePrice = 1000;
+            //computerPrice = 2000;
+            //tvPrice = 5000;
+
+            //int shoeCount, computerCount, tvCount;
+            //Console.Write("lütfen aldığınız ayakkabı sayısını giriniz. ");
+            //shoeCount = int.Parse(Console.ReadLine());
+
+            //int totalPrice = shoeCount * shoePrice;
+
+            //Console.WriteLine();
+            //Console.WriteLine("toplam ödemeniz gereken tutar");
+
+            #endregion
+
+
+
+
+
+            Console.Read();
+
+        }
+    }
+}
